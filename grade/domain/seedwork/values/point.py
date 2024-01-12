eae29dd645ecd78245b7eabb2b05206ca12f5f21
Ivan Zakrevsky <ivzak@yandex.ru>
2024-01-12 15:56:21 +0300

    Add EmptyPoint

--- grade/domain/seedwork/values/point.py
+++ grade/domain/seedwork/values/point.py
@@ -20,6 +20,16 @@ class Point(_Point):
         exporter.latitude = Decimal(self.latitude).quantize(Decimal(".000001"))
         exporter.altitude = bool(self.altitude) and Decimal(self.altitude).quantize(Decimal(".000001")) or None
 
+    @property
+    def is_empty(self):
+        return self == EmptyPoint()
+
+
+class EmptyPoint(Point):
+
+    def __new__(cls):
+        return _Point.__new__(cls)
+
 
 class IPointExporterSetter(metaclass=ABCMeta):
 
